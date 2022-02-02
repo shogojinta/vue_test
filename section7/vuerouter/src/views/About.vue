4a96d894f478bdc534a886155feaa5aed87838b1
jinta <jinta.shogo@home-net.gr.jp>
2022-02-02 18:52:48 +0900

    88

--- section7/vuerouter/src/views/About.vue
+++ section7/vuerouter/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="checkRouteInfo">ルート情報</button>
+    <button @click="goToHome">Home</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "About",
+    methods: {
+      checkRouteInfo() {
+        console.log(this.$route);
+        console.log(this.$route.path);
+        console.log(this.$router);
+      },
+      goToHome() {
+        this.$router.push("/")
+      }
+    },
+  }
+</script>

023297f4642c4b142b18c6504f45346a6c3b74e4
jinta <jinta.shogo@home-net.gr.jp>
2022-02-04 08:47:13 +0900

    106

---
+++ section8/bookapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <Header/>
+      <v-main>
+        <router-view/>
+      </v-main>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/global/Header.vue';
+import Footer from '@/global/Footer.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+
+};
+</script>
